faa2a3e718260acc9ebaf3c3da5a4eb78672fd45
Lexusbat <skye.lexus205@gmail.com>
2026-07-24 14:55:10 +0200

    Intro Page setup

---
+++ flutter_sushi_app/lib/Pages/intro_pages.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+
+@override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+
+
+
+
+
+
+}
